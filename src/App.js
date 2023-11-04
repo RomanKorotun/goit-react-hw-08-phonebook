a3d461eece4fc557f4c71f5cb01d114d05b11643
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-11-04 23:58:41 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -5,38 +5,8 @@ import { ContactsList } from 'components/Contacts/Contacts';
 import { Layout, TitleH1, TitleH2 } from 'Layout';
 import { useSelector } from 'react-redux';
 
-// const getContactsLs = () => {
-//   const contactsLs = localStorage.getItem('contacts');
-//   if (contactsLs !== null) {
-//     return JSON.parse(contactsLs);
-//   }
-//   return [];
-// };
-
 export const App = () => {
-  // const [contacts, setContacts] = useState(getContactsLs);
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const addContact = newContact => {
-  //   setContacts(prevState => [...prevState, { ...newContact, id: nanoid() }]);
-  // };
-
-  // const filterValue = target => {
-  //   setFilter(target);
-  // };
-
-  // const deleteContact = id => {
-  //   setContacts(prevState => prevState.filter(contact => contact.id !== id));
-  // };
-
   const contacts = useSelector(state => state.contactsKey.contacts);
-  // const visibleItems = contacts.filter(contact =>
-  //   contact.name.toLowerCase().includes(filter.toLowerCase())
-  // );
   return (
     <React.Fragment>
       <Layout>
